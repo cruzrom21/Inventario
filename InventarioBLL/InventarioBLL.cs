b5eb4719aeb8d95b9f1c446f9b1be37e55032342
LUIGI DARYANEI CRUZ ROMERO <l.cruzromero@dxc.com>
2023-04-04 17:01:19 -0500

    Firt Commit

---
+++ InventarioBLL/InventarioBLL.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioBLL
+{
+    public class Inventario
+    {
+        private readonly InventarioContext _context;
+        public Inventario(InventarioContext _context)
+        {
+            this._context = _context;
+        }
+
+        public string DisponibilidadNeta(string sku_id)
+        {
+            try
+            {
+               Total inventario -(Unidades comprometidas + UnidadesUbicacion despachadas)
+
+                string JsonString = JsonConvert.SerializeObject(new
+                {
+                    products = list,
+                    status = "success"
+                });
+
+                return JsonString;
+            }
+            catch (Exception ex)
+            {
+                string JsonString = JsonConvert.SerializeObject(new
+                {
+                    status = "Ocurrio un problema" + ex.Message
+                });
+
+                return JsonString;
+            }
+        }
+
+
+        public string TotalInventario(string sku_id)
+        {
+            try
+            {
+                
+                Unidades comprometidas + UnidadesUbicacion despachadas + unidades si ubicacion 
+
+                string JsonString = JsonConvert.SerializeObject(new
+                {
+                    products = list,
+                    status = "success"
+                });
+
+                return JsonString;
+            }
+            catch (Exception ex)
+            {
+                string JsonString = JsonConvert.SerializeObject(new
+                {
+                    status = "Ocurrio un problema" + ex.Message
+                });
+
+                return JsonString;
+            }
+        }
+
+
+        public string UnidadesUbicacion(string sku_id)
+        {
+            try
+            {
+                cantidad reserva cantidad activo cantidad almacenada 
+                string JsonString = JsonConvert.SerializeObject(new
+                {
+                    products = list,
+                    status = "success"
+                });
+
+                return JsonString;
+            }
+            catch (Exception ex)
+            {
+                string JsonString = JsonConvert.SerializeObject(new
+                {
+                    status = "Ocurrio un problema" + ex.Message
+                });
+
+                return JsonString;
+            }
+        }
+    }
+}
---
+++ InventarioPrueba/Controllers/InventarioController.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TiendaVirtual.Controllers
+{
+	[ApiController]
+	[Route("api/inventario")]
+	public class InventarioController : Controller
+	{
+		//private readonly TiendaVirtualContext _context;
+		//public InventarioController(TiendaVirtualContext _context)
+		//{
+		//	this._context = _context;
+		//}
+
+		[HttpPost]
+        [Route("api/inventario/disponibilidad")]
+        public async Task<string> PostDisponibilidadNeta(string sku_id)
+		{
+			BuyBLL buy = new BuyBLL(_context);
+			string response = buy.SaveBuy(resquest);
+
+			return response;
+		}
+
+        [HttpPost]
+        [Route("api/inventario/inventario")]
+        public async Task<string> PostInventario(string sku_id)
+        {
+            BuyBLL buy = new BuyBLL(_context);
+            string response = buy.SaveBuy(resquest);
+
+            return response;
+        }
+
+        [HttpPost]
+        [Route("api/inventario/unidades")]
+        public async Task<string> PostUnidades(string sku_id)
+        {
+            BuyBLL buy = new BuyBLL(_context);
+            string response = buy.SaveBuy(resquest);
+
+            return response;
+        }
+    }
+}
